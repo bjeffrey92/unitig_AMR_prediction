4ffd25ae0cfe1624bb58253a7ebbe3e4ea7e35c2
Ben <bjeffrey92@hotmail.com>
2020-10-28 18:17:46 +0000

    only include unititgs of intermediate frequency in the adjacency graph

--- scripts/cDB_graph_adjacency_matrix.py
+++ scripts/cDB_graph_adjacency_matrix.py
@@ -1,13 +1,14 @@
 import pyfrost #https://github.com/broadinstitute/pyfrost
-# import tensorflow as tf
-# import numpy as np
+import torch
+import numpy as np
+import csv
 from scipy import sparse 
+from itertools import compress
+from scripts.parse_features import parse_metadata
+import logging
 
-gfa_file = 'data/gonno_unitigs/gonno_unitigs.gfa'
-unitigs_fasta = 'data/gonno_unitigs/gonno_unitigs_unitigs.fasta'
-
-g = pyfrost.load(gfa_file) #path to bfg_colours file is inferred
-
+logging.basicConfig()
+logging.root.setLevel(logging.INFO)
 
 def map_nodes(node_list, nodes_dict):    
     node_uuids = []
@@ -18,7 +19,48 @@ def map_nodes(node_list, nodes_dict):
     return node_uuids
 
 
+def filter_unitigs(rtab_file, files_to_include, filt = (0.01, 0.99)):
+
+    with open(rtab_file, 'r') as a:
+        reader = csv.reader(a, delimiter = '\t')
+        header = next(reader)
+        file_filter = [i in files_to_include for i in header] #which to include
+        intermediate_unitigs = []
+        for row in reader:
+            pattern_id = row[0]
+            row = list(compress(row, file_filter))
+            frequency = sum([1 for i in row if i == '1'])/len(row)
+            if frequency >= filt[0] and frequency <= filt[1]:
+                intermediate_unitigs.append(pattern_id)
+
+    return intermediate_unitigs
+
+
+def convert_to_tensor(adj_matrix):
+    values = adj_matrix.data
+    indices = np.vstack((adj_matrix.row, adj_matrix.col))
+
+    i = torch.LongTensor(indices)
+    v = torch.FloatTensor(values)
+    shape = adj_matrix.shape
+
+    return torch.sparse_coo_tensor(i, v, torch.Size(shape))
+
+
 if __name__ == '__main__':
+
+    gfa_file = 'data/gonno_unitigs/gonno_unitigs.gfa'
+    unitigs_fasta = 'data/gonno_unitigs/gonno_unitigs_unitigs.fasta'
+    rtab_file = 'data/gonno_unitigs/gonno.rtab' #to filter unitigs based on frequency
+    metadata_file = 'data/metadata.csv'
+
+    g = pyfrost.load(gfa_file) #path to bfg_colours file is inferred
+
+    #applies frequency filter to unitigs
+    metadata = parse_metadata(metadata_file, rtab_file) #to know which files to include
+    intermediate_unitigs = filter_unitigs(rtab_file, metadata.index) 
+
+    logging.info('Identifying intermediate frequency nodes and mapping them to uuids, this could take a while')
     #map unitig sequences to their uuid
     nodes_dict = {}
     with open(unitigs_fasta, 'r') as a:
@@ -27,9 +69,11 @@ if __name__ == '__main__':
                 kmer = line.strip('>*\n')
                 continue
             else: 
-                nodes_dict[line.strip('\n')] = kmer
+                if kmer in intermediate_unitigs:
+                    nodes_dict[line.strip('\n')] = kmer
 
-    #get adjacency of all nodes in unitigs.fasta
+    logging.info('Extracting adjacency')
+    #get adjacency of all intermediate nodes in unitigs.fasta
     adjacency_dict = {}
     for n, neighbours in g.adj.items():
         node = g.nodes[n]['unitig_sequence']
@@ -45,12 +89,15 @@ if __name__ == '__main__':
     dims = len(unitig_adjacency)
     adj_matrix = sparse.dok_matrix((dims, dims)) #empty sparse matrix 
 
+    logging.info('Constructing sparse adjacency matrix')
     #fill in sparse matrix
+    unitig_order = list(unitig_adjacency.keys())
     for unitig, neighbours in unitig_adjacency.items():
         for nbr in neighbours:
-            adj_matrix[unitig, nbr] = 1
-            adj_matrix[nbr, unitig] = 1 
+            unitig_pos = unitig_order.index(unitig)
+            nbr_pos = unitig_order.index(nbr)
+            adj_matrix[unitig_pos, nbr_pos] = 1
+            adj_matrix[nbr_pos, unitig_pos] = 1 
 
-    adj_matrix = adj_matrix.tocoo()
-    sparse.save_npz('cDB_graph_adjacency_matrix.npz', adj_matrix)
-
+    logging.info('Converting adjacency matrix to sparse ten')
+    adj_tensor = convert_to_tensor(adj_matrix.tocoo())
