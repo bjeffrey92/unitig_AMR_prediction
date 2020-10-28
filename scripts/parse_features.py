62fe457c6bd6651a5275f1004cf37fde6db510fa
Ben <bjeffrey92@hotmail.com>
2020-10-28 18:18:37 +0000

    build sparse tensors of features and labels from intermediate frequency unitigs

--- scripts/parse_features.py
+++ scripts/parse_features.py
@@ -4,12 +4,10 @@ import numpy as np
 import torch
 import os
 import logging
+from itertools import compress
 
 logging.basicConfig()
 logging.root.setLevel(logging.INFO)
-
-rtab_file = 'data/gonno_unitigs/gonno.rtab'
-metadata_file = 'data/metadata.csv'
 
 def load_features(rtab_file):
     '''
@@ -48,11 +46,13 @@ def load_features(rtab_file):
 
     del values
 
-    features = torch.sparse.FloatTensor(indices, values_tensor, shape)
+    features = torch.sparse_coo_tensor(indices, values_tensor, shape)
     return features
 
 
-def split_training_and_testing(rtab_file, training_split = 0.7):
+def split_training_and_testing(rtab_file, 
+                                files_to_include, 
+                                training_split = 0.7):
     '''
     create training and testing rtab files so features can be generated in most memory efficient way
     '''
@@ -63,6 +63,9 @@ def split_training_and_testing(rtab_file, training_split = 0.7):
     with open(rtab_file, 'r') as a:
         reader = csv.reader(a, delimiter = '\t')
         header = next(reader)
+        filt = [i in files_to_include for i in header] #which to include
+        filt[0] = True #add True at start to include pattern_id column
+        header = list(compress(header, filt))
         total_samples = len(header) - 1
 
         training_n = round(total_samples * training_split)
@@ -78,6 +81,7 @@ def split_training_and_testing(rtab_file, training_split = 0.7):
         j = 1
         for row in reader:
             logging.info(f'processing {j} of {num_unitigs} unitigs')
+            row = list(compress(row, filt))
             j += 1
 
             frequency = sum([1 for i in row[1:] if i == '1'])/len(row[1:])
@@ -108,7 +112,7 @@ def split_training_and_testing(rtab_file, training_split = 0.7):
     return training_data_file, testing_data_file
 
 
-def load_labels(metadata_file, rtab_file):
+def parse_metadata(metadata_file, rtab_file):
     metadata_df = pd.read_csv(metadata_file)
 
     with open(rtab_file, 'r') as a:
@@ -118,21 +122,49 @@ def load_labels(metadata_file, rtab_file):
     assert all([i.endswith('.contigs_velvet') for i in input_files])    
     accessions = [i.strip('.contigs_velvet') for i in input_files]
 
-    accessions = pd.Dataframe(accessions)
+    accessions = pd.DataFrame(accessions)
     df = metadata_df.merge(accessions, 
                         left_on = 'Sanger_lane', right_on = 0)
     df = df.rename(columns = {0:'Filename'})
-    df['Filename'] = df['Filename'] + '.contigs_velvet'
+    df['Filename'] = df['Filename'] + '.contigs_velvet' #so can identify relevant inputs in the rtab
 
     diff = len(accessions) - len(df)
     if diff > 0:
         logging.warning(f'{diff} entries in {rtab_file} could not be mapped to entries in {metadata_file}')
+        input_files = [i for i in input_files if i in df.Filename.values] #get all which are present
+
+    df.set_index('Filename', inplace = True)
+    df = df.loc[input_files] #order metadata by order that files are present in the rtab
 
     return df
 
+
+def load_labels(metadata, label_column, rtab_file):
+
+    with open(rtab_file, 'r') as a:
+        reader = csv.reader(a, delimiter = '\t')
+        input_files = next(reader)[1:]
+
+    metadata = metadata.loc[input_files] #order dataframe 
+    
+    return torch.FloatTensor(metadata[label_column].values)
+
+
 if __name__ == '__main__':
 
-    training_rtab_file,testing_rtab_file = split_training_and_testing(rtab_file)
+    rtab_file = 'data/gonno_unitigs/gonno.rtab'
+    metadata_file = 'data/metadata.csv'
 
+    #maps entries in rtab to metadata
+    metadata = parse_metadata(metadata_file, rtab_file)
+
+    training_rtab_file, testing_rtab_file = \
+                split_training_and_testing(rtab_file, metadata.index)
+
+    #reads in rtab as sparse feature tensor
     training_features = load_features(training_rtab_file)
     testing_features = load_features(testing_rtab_file)
+
+    #parse training and testing labels as tensors
+    training_labels = load_labels(metadata, 'log2_cip_mic', training_rtab_file)
+    testing_labels = load_labels(metadata, 'log2_cip_mic', testing_rtab_file)
