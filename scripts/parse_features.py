d40485b2f82bfae7456943b570d9eb178b5897e9
Ben <bjeffrey92@hotmail.com>
2020-10-28 11:06:24 +0000

    functio to split rtab into training and testing sets with unitigs of intermediate frequency

--- scripts/parse_features.py
+++ scripts/parse_features.py
@@ -2,7 +2,12 @@ import pandas as pd
 import csv
 import numpy as np
 import torch
-import logging 
+import os
+import logging
+
+logging.basicConfig()
+logging.root.setLevel(logging.NOTSET)
+
 
 rtab_file = 'data/gonno_unitigs/gonno.rtab'
 metadata_file = 'data/metadata.csv'
@@ -21,7 +26,7 @@ def load_features(rtab_file):
     
         x_idx = []
         y_idx = []
-        # values = []
+        values = []
 
         i = 0
         for row in reader:
@@ -29,16 +34,80 @@ def load_features(rtab_file):
                 if row[j] == '1':
                     x_idx.append(j - 1)
                     y_idx.append(i)
-                    # values.append(1)
-            print(f'{i}/{num_unitigs} unitigs processed')
+                    values.append(1)
+            logging.info(f'{i}/{num_unitigs} unitigs processed')
             i += 1
 
-    shape = (num_samples, num_unitigs)
     indices = torch.LongTensor([x_idx, y_idx])
-    values = torch.FloatTensor(values)
-    return torch.sparse.FloatTensor(indices, 
-                                    values,
-                                    shape)
+    
+    #delete these to free up RAM
+    del x_idx
+    del y_idx
+
+    shape = (num_samples, num_unitigs)
+    values_tensor = torch.FloatTensor(values)
+
+    del values
+
+    features = torch.sparse.FloatTensor(indices, values_tensor, shape)
+    return features
+
+
+def split_training_and_testing(rtab_file, training_split = 0.7):
+    '''
+    create training and testing rtab files so features can be generated in most memory efficient way
+    '''
+
+    num_unitigs = sum(1 for line in open(rtab_file)) - 1
+
+    #get training and testing data as separate lists
+    with open(rtab_file, 'r') as a:
+        reader = csv.reader(a, delimiter = '\t')
+        header = next(reader)
+        total_samples = len(header) - 1
+
+        training_n = round(total_samples * training_split)
+        testing_n = total_samples - training_n
+
+        #memory allocation
+        training_rows = [header[:training_n + 1]] + \
+            ([[None] * (training_n + 1)] * num_unitigs)
+        testing_rows = [header[:1] + header[training_n + 1:]] + \
+            ([[None] * (testing_n + 1)] * num_unitigs)
+
+        i = 1
+        j = 1
+        for row in reader:
+            logging.info(f'processing {j} of {num_unitigs} unitigs')
+            j += 1
+
+            frequency = sum([int(i) for i in row[1:] if i == '1'])/len(row[1:])
+            if frequency < 0.01 or frequency > 0.99: continue #only include intermediate frequency unitigs
+
+            training_rows[i] = row[:training_n + 1]
+            testing_rows[i] = row[:1] + row[training_n + 1:]
+            
+            i += 1
+
+    training_data_file = os.path.join(os.path.dirname(rtab_file), 
+                            'training_data_' + os.path.basename(rtab_file))
+    testing_data_file = os.path.join(os.path.dirname(rtab_file), 
+                            'testing_data_' + os.path.basename(rtab_file))
+
+    logging.info(f'training data being written to {training_data_file}')
+    with open(training_data_file, 'w', newline = '') as csvfile:
+        writer = csv.writer(csvfile, delimiter = '\t')
+        for row in training_rows[:i]:
+            writer.writerow(row)
+
+    logging.info(f'testing data being written to {testing_data_file}')
+    with open(testing_data_file, 'w', newline = '') as csvfile:
+        writer = csv.writer(csvfile, delimiter = '\t')
+        for row in testing_rows[:i]:
+            writer.writerow(row)
+
+    return training_data_file, testing_data_file
+
 
 def load_labels(metadata_file, rtab_file):
     metadata_df = pd.read_csv(metadata_file)
