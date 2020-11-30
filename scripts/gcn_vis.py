a4e193c3614c7c71ed92f83b89441359f1eb0df2
Ben <bjeffrey92@hotmail.com>
2020-11-30 14:26:25 +0000

    refactoring and adding symmetric normalised adjacency matrix

--- scripts/gcn_vis.py
+++ scripts/gcn_vis.py
@@ -1,55 +1,66 @@
 import pickle
 import random
+import math
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 
-def colour_code(node, max_attr):
-    '''
-    Converts node value to a hex code with colour relative to max node value 
-    '''
-    N = 255/max_attr
-    hex_code = '#%02x%02x%02x' % (int(N * node), int(255 - N * node), 255) 
+def colour_code(N, max_attr):
+    i = 255/max_attr
+    hex_code = '#%02x%02x%02x' % (int(i * N), int(255 - i * N), 255) 
     return hex_code
 
-
-def coloured_plotting(G):
-    color_map = [colour_code(G.nodes[node]['attr']) for node in G]
-    nx.draw(G, node_color = color_map, with_labels = False)
+def plot_graph(graph, layout, max_attr):
+    G = nx.Graph(graph) #shallow copy
+    colour_map = [colour_code(G.nodes[node]['attr'], max_attr) for node in G]
+    nx.draw(G, pos = layout, node_color = colour_map,  with_labels = False)
     plt.show()
 
-def get_attribute_vector(G):
-    attributes = []
-    for node in G:
-        attributes.append(G.nodes[node]['attr'])
-    return np.array(attributes)
+def get_attribute_vector(graph):
+    return np.array([graph.nodes[node]['attr'] for node in graph])
 
-def set_attributes(G, attributes):
+def set_attributes(graph, attributes):
     assert len(attributes) == len(G)
     i = 0
-    for node in G:
-        G.nodes[node]['attr'] = float(attributes[i])
+    for node in graph:
+        graph.nodes[node]['attr'] = float(attributes[i])
         i += 1
-    return G
+    return graph
 
-def convolve(G, adj = adj):
-    attributes = get_attribute_vector(G)
-    new_attributes = np.linalg.linalg.matmul(adj, attributes)
-    # normed_attributes = new_attributes/max(new_attributes)
-    G = set_attributes(G, new_attributes)
-    return G
+def convolve(graph, adj, deg = None):
+    attributes = get_attribute_vector(graph)
+    if deg is None:
+        new_attributes = np.linalg.multi_dot([adj, attributes])
+    else:
+        new_attributes = np.linalg.multi_dot([deg, adj, deg, attributes])
+    return set_attributes(graph, new_attributes)
 
 if __name__ == '__main__':
-    with open('small_graph.pkl', 'rb') as a:
+    with open('smaller_graph.pkl', 'rb') as a:
         G = pickle.load(a)
 
     adj = nx.adj_matrix(G)
     adj = adj.toarray()
+    I = np.eye(adj.shape[0])
+    adj = adj + I #add self loops to each node when convolving
+
+    degs = np.array([v for k,v in dict(nx.degree(G)).items()]) #degree of each node
+    normed_deg = np.array([1/math.sqrt(i) for i in degs]) #equivalent to raising degree matrix to power -1/2
+    deg = np.diag(normed_deg)
 
     #label nodes with 1/5 probability of being labelled 1
     for node in G:
         G.nodes[node]['attr'] = np.random.binomial(1, 0.2)
 
-    coloured_plotting(G)
+    layout = nx.spring_layout(G) #to keep layout constant
+    
+    G_1 = convolve(nx.Graph(G), adj)
+    G_2 = convolve(nx.Graph(G), adj, deg)
 
-    G = convolve(G, adj)
+    max_attr = max([G.nodes[node]['attr'] for node in G] + 
+                    [G_1.nodes[node]['attr'] for node in G_1] + 
+                    [G_2.nodes[node]['attr'] for node in G_2])
+
+    plot_graph(G, layout, max_attr)
+    plot_graph(G_1, layout, max_attr)
+    plot_graph(G_2, layout, max_attr)
