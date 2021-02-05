0df6fa95361be0408d070fdf55a798e48a94578e
Ben <bjeffrey92@hotmail.com>
2021-02-05 15:42:25 +0000

    refactoring to do phylogenetic cross validation

--- lasso_model/lasso_model.py
+++ lasso_model/lasso_model.py
@@ -4,10 +4,11 @@ import torch
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Lasso
 from sklearn.exceptions import ConvergenceWarning
-from numpy import linspace
+from numpy import linspace, sort, array_equal
 
 from lasso_model.utils import load_training_data, load_testing_data, \
-                            load_adjacency_matrix, accuracy, convolve
+                            load_adjacency_matrix, accuracy, convolve, \
+                            load_metadata
 
 import pickle
 import os 
@@ -18,9 +19,15 @@ logging.basicConfig()
 logging.root.setLevel(logging.INFO)
 
 
-def CV(training_features, training_labels, 
-        testing_features, testing_labels, alphas: list, adj = None) -> dict:
-    
+def fit_model_by_grid_search(training_features, training_labels, 
+                            testing_features, testing_labels, 
+                            validation_features, validation_labels, 
+                            alphas: list, adj = None) -> dict:
+    '''
+    Training and testing data is random split of part of the tree, 
+    validation data is from a separate clade
+    '''
+
     accuracy_dict = {i:None for i in alphas}
 
     if adj is not None:
@@ -53,15 +60,72 @@ def CV(training_features, training_labels,
 
         training_predictions = reg.predict(training_features)
         testing_predictions = reg.predict(testing_features)
+        validation_predictions = reg.predict(validation_features)
 
         training_accuracy = accuracy(torch.tensor(training_predictions), 
                                     training_labels)
         testing_accuracy = accuracy(torch.tensor(testing_predictions), 
                                     testing_labels)
+        validation_accuracy = accuracy(torch.tensor(validation_predictions),
+                                        validation_labels)
 
-        accuracy_dict[a] = [training_accuracy, testing_accuracy]
+        accuracy_dict[a] = {'training_accuracy': training_accuracy, 
+                            'testing_accuracy': testing_accuracy, 
+                            'validation_accuracy': validation_accuracy}
 
     return accuracy_dict
+
+
+def leave_one_out_CV(training_data, testing_data, 
+                    training_metadata, testing_metadata):
+
+    clades = training_metadata.Clade.unique()
+    assert array_equal(sort(clades), sort(testing_metadata.Clade.unique())), \
+        'Different clades found in training and testing metadata'
+
+    alphas = linspace(0.01, 0.1, 10)
+    
+    results_dict = {}
+    for left_out_clade in clades:
+        logging.info(
+            f'Formatting data for model with clade {left_out_clade} left out')
+        training_indices = training_metadata.loc[
+                            training_metadata.Clade != left_out_clade].index
+        testing_indices = testing_metadata.loc[
+                            testing_metadata.Clade != left_out_clade].index
+        validation_indices_1 = training_metadata.loc[
+                            training_metadata.Clade == left_out_clade].index #extract data from training set
+        validation_indices_2 = testing_metadata.loc[
+                            testing_metadata.Clade == left_out_clade].index #extract data from testing set
+
+        training_features = torch.index_select(training_data[0], 0, 
+                                            torch.tensor(training_indices))
+        training_labels = torch.index_select(training_data[1], 0, 
+                                            torch.tensor(training_indices))
+        testing_features = torch.index_select(testing_data[0], 0,
+                                            torch.tensor(testing_indices))
+        testing_labels = torch.index_select(testing_data[1], 0, 
+                                            torch.tensor(testing_indices))
+        validation_features = torch.cat([
+                                torch.index_select(training_data[0], 0, 
+                                            torch.tensor(validation_indices_1)),
+                                torch.index_select(testing_data[0], 0, 
+                                            torch.tensor(validation_indices_2))
+                                ])
+        validation_labels = torch.cat([
+                                torch.index_select(training_data[1], 0, 
+                                            torch.tensor(validation_indices_1)),
+                                torch.index_select(testing_data[1], 0,
+                                            torch.tensor(validation_indices_2))
+                                ])
+
+        accuracy_dict = fit_model_by_grid_search(training_features, training_labels, 
+                                testing_features, testing_labels, 
+                                validation_features, validation_labels,
+                                alphas)
+        results_dict[left_out_clade] = accuracy_dict
+
+    return results_dict
 
 
 def save_output(accuracy_dict, fname):
@@ -71,13 +135,19 @@ def save_output(accuracy_dict, fname):
 
 def plot_results(accuracy_dict, fname):
 
-    training_acc = [i[0] for i in accuracy_dict.values()]
-    testing_acc = [i[1] for i in accuracy_dict.values()]
+    training_acc = [i['training_accuracy'] for i in accuracy_dict.values()]
+    testing_acc = [i['testing_accuracy'] for i in accuracy_dict.values()]
+    validation_acc = [i['validation_accuracy'] for i in accuracy_dict.values()]
 
     plt.clf()
 
-    plt.scatter(list(accuracy_dict.keys()), training_acc)
-    plt.scatter(list(accuracy_dict.keys()), testing_acc)
+    plt.scatter(list(accuracy_dict.keys()), training_acc, 
+                label = 'Training Data Accuracy')
+    plt.scatter(list(accuracy_dict.keys()), testing_acc, 
+                label = 'Testing Data Accuracy')
+    plt.scatter(list(accuracy_dict.keys()), validation_acc, 
+                label = 'Left Out Clade Accuracy')
+    plt.legend(loc = 'lower left')
 
     plt.savefig(fname)
 
@@ -89,17 +159,19 @@ if __name__ == '__main__':
     for outcome in outcomes:
         data_dir = os.path.join(root_dir, outcome)
 
-        training_features, training_labels = load_training_data(data_dir)
-        testing_features, testing_labels = load_testing_data(data_dir)
-        adj = load_adjacency_matrix(data_dir)
+        training_data = load_training_data(data_dir)
+        testing_data = load_testing_data(data_dir)
+        training_metadata, testing_metadata = load_metadata(data_dir)        
 
-        alphas = linspace(0.01, 0.1, 10)
+        results_dict = leave_one_out_CV(training_data, 
+                                        testing_data,
+                                        training_metadata, 
+                                        testing_metadata)
 
-        accuracy_dict = CV(training_features, training_labels, 
-                    testing_features, testing_labels, alphas)
+        fname = outcome + '_CV_lasso_predictions.pkl'
+        save_output(results_dict, fname)
 
-        fname = outcome + 'convolved_degree_normalised_lasso_predictions.pkl'
-        save_output(accuracy_dict, fname)
-
-        fname = outcome + 'convolved_degree_normalised_lasso_predictions.png'
-        plot_results(accuracy_dict, fname)
+        for left_out_clade in results_dict.keys():
+            fname = outcome + \
+                f'_validation_clade_{left_out_clade}_lasso_predictions.png'
+            plot_results(results_dict[left_out_clade], fname)
--- lasso_model/utils.py
+++ lasso_model/utils.py
@@ -13,12 +13,19 @@ def load_testing_data(data_dir):
     labels = torch.load(os.path.join(data_dir, 'testing_labels.pt'))
     return features.to_dense(), labels
 
+def load_metadata(data_dir):
+    training_metadata = pd.read_csv(
+                            os.path.join(data_dir, 'training_metadata.csv'))
+    testing_metadata = pd.read_csv(
+                            os.path.join(data_dir, 'testing_metadata.csv'))
+    return training_metadata, testing_metadata
+
 def accuracy(predictions: torch.tensor, labels: torch.tensor):
     diff = predictions - labels
     correct = diff[[i < 1 for i in diff]]
     return len(correct)/len(predictions) * 100
 
-def load_adjacency_matrix(data_dir, degree_normalised = True):
+def load_adjacency_matrix(data_dir, degree_normalised = False):
     if degree_normalised:
         adj = torch.load(os.path.join(data_dir, 
                                 'degree_normalised_unitig_adjacency_tensor.pt'))
