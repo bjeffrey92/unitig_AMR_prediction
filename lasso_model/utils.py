2d340ef6f0e805fd5267a1bdfca76bcafa4e295f
Ben <bjeffrey92@hotmail.com>
2021-02-10 09:51:36 +0000

    caching

--- lasso_model/utils.py
+++ lasso_model/utils.py
@@ -3,16 +3,19 @@ import os
 from functools import lru_cache
 import pandas as pd
 
+@lru_cache(maxsize = 1)
 def load_training_data(data_dir):
     features = torch.load(os.path.join(data_dir, 'training_features.pt'))
     labels = torch.load(os.path.join(data_dir, 'training_labels.pt'))
     return features.to_dense(), labels
 
+@lru_cache(maxsize = 1)
 def load_testing_data(data_dir):
     features = torch.load(os.path.join(data_dir, 'testing_features.pt'))
     labels = torch.load(os.path.join(data_dir, 'testing_labels.pt'))
     return features.to_dense(), labels
 
+@lru_cache(maxsize = 1)
 def load_metadata(data_dir):
     training_metadata = pd.read_csv(
                             os.path.join(data_dir, 'training_metadata.csv'))
