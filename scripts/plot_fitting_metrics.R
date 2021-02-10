c0eddee5702d8b4865c80491a7931d744d933e77
Ben <bjeffrey92@hotmail.com>
2021-02-10 16:52:40 +0000

    plotting validation data also

--- scripts/plot_fitting_metrics.R
+++ scripts/plot_fitting_metrics.R
@@ -9,11 +9,16 @@ training_data_loss <- df$training_data_loss
 training_data_accuracy <- df$training_data_acc
 testing_data_loss <- df$testing_data_loss
 testing_data_accuracy <- df$testing_data_acc
+validation_data_loss <- df$validation_data_loss
+validation_data_accuracy <- df$validation_data_acc
+
 
 png('progress_plots.png')
-par(mfrow=c(2,2))
+par(mfrow=c(3,2))
 plot(training_data_loss)
 plot(training_data_accuracy)
-# plot(testing_data_loss)
-# plot(testing_data_accuracy)
+plot(testing_data_loss)
+plot(testing_data_accuracy)
+plot(validation_data_loss)
+plot(validation_data_accuracy)
 dev.off()
