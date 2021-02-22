231d8064ed20ff8f3ad7fe8448b5306e55166428
Ben <bjeffrey92@hotmail.com>
2021-02-22 13:36:29 +0000

    saving embedding labelled with node names

--- scripts/umap_of_unitigs.py
+++ scripts/umap_of_unitigs.py
@@ -4,8 +4,9 @@ import os
 import numpy as np
 import re
 import matplotlib.pyplot as plt
+import pandas as pd
 
-from GNN_model.utils import R_or_S, breakpoints
+from GNN_model.utils import R_or_S, breakpoints, load_metadata
 
 def load_data(data_dir):
     train_features = torch.load(os.path.join(data_dir, 'training_features.pt'))
@@ -21,7 +22,9 @@ def load_data(data_dir):
     features = np.array(train_features + test_features)
     labels = np.array(train_labels + test_labels)
 
-    return features, labels
+    metadata = pd.concat(list(load_metadata(data_dir)))
+    
+    return features, labels, metadata.Sanger_lane.to_numpy()
 
 
 def main(Ab):
@@ -30,15 +33,21 @@ def main(Ab):
     pat = r'.*?\_(.*)_.*'
     ab_name = re.findall(pat, Ab)[0]
 
-    features, labels = load_data(data_dir)
+    features, labels, ids = load_data(data_dir)
     r_s_labels = R_or_S(labels, breakpoints[ab_name])
 
-    reducer = umap.UMAP()
+    reducer = umap.UMAP(metric = 'jaccard')
     embedding = reducer.fit_transform(features)
 
+    embedding_df = pd.DataFrame(embedding)
+    embedding_df['labels'] = labels
+    embedding_df['r_s_labels'] = r_s_labels
+    embedding_df['ids'] = ids
+    embedding_df.to_csv(f'umap_results/jaccard_distance/{Ab}_umap_embedding.csv')
+
     plt.clf()
-    plt.scatter(x = embedding[:,0], y = embedding[:,1], c = r_s_labels)
-    plt.savefig(f'{Ab}_umap_coloured_by_resistance_status.png')
+    plt.scatter(x = embedding_df[0], y = embedding_df[1], c = embedding_df.r_s_labels)
+    plt.savefig(f'umap_results/jaccard_distance/{Ab}_umap_coloured_by_resistance_status.png')
 
 
 if __name__ == '__main__':
