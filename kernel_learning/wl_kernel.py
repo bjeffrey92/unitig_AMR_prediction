a1a376ad822673aefb18374dad081dd0db4c4456
Ben <bjeffrey92@hotmail.com>
2021-03-16 10:54:06 +0000

    autoformatting

--- kernel_learning/wl_kernel.py
+++ kernel_learning/wl_kernel.py
@@ -6,126 +6,207 @@ import logging
 import os
 import torch
 import sys
-import pandas as pd
 
-from GNN_model.utils import load_adjacency_matrix, load_training_data, \
-                            load_testing_data, load_metadata
+from GNN_model.utils import (
+    load_adjacency_matrix,
+    load_training_data,
+    load_testing_data,
+    load_metadata,
+)
 
 logging.basicConfig()
 logging.root.setLevel(logging.INFO)
 
 
-def train_test_validation_split(left_out_clade, training_features, 
-                                    testing_features, training_metadata, 
-                                    testing_metadata):
+def train_test_validation_split(
+    left_out_clade,
+    training_features,
+    testing_features,
+    training_metadata,
+    testing_metadata,
+):
     logging.info(
-        f'Formatting data for model with clade {left_out_clade} left out')
+        f"Formatting data for model with clade {left_out_clade} left out"
+    )
     training_indices = training_metadata.loc[
-                        training_metadata.clusters != left_out_clade].index
+        training_metadata.clusters != left_out_clade
+    ].index
     testing_indices = testing_metadata.loc[
-                        testing_metadata.clusters != left_out_clade].index
+        testing_metadata.clusters != left_out_clade
+    ].index
     validation_indices_1 = training_metadata.loc[
-                        training_metadata.clusters == left_out_clade].index #extract data from training set
+        training_metadata.clusters == left_out_clade
+    ].index  # extract data from training set
     validation_indices_2 = testing_metadata.loc[
-                        testing_metadata.clusters == left_out_clade].index #extract data from testing set
+        testing_metadata.clusters == left_out_clade
+    ].index  # extract data from testing set
 
-    clade_training_features = torch.index_select(training_features, 0, 
-                                        torch.as_tensor(training_indices, 
-                                                    dtype = torch.int64))
-    clade_testing_features = torch.index_select(testing_features, 0, 
-                                        torch.as_tensor(testing_indices,
-                                                    dtype = torch.int64))    
-    validation_features = torch.cat([
-                            torch.index_select(training_features, 0, 
-                                        torch.as_tensor(validation_indices_1,
-                                                    dtype = torch.int64)),
-                            torch.index_select(testing_features, 0, 
-                                        torch.as_tensor(validation_indices_2,
-                                                    dtype = torch.int64))
-                            ])
+    clade_training_features = torch.index_select(
+        training_features,
+        0,
+        torch.as_tensor(training_indices, dtype=torch.int64),
+    )
+    clade_testing_features = torch.index_select(
+        testing_features,
+        0,
+        torch.as_tensor(testing_indices, dtype=torch.int64),
+    )
+    validation_features = torch.cat(
+        [
+            torch.index_select(
+                training_features,
+                0,
+                torch.as_tensor(validation_indices_1, dtype=torch.int64),
+            ),
+            torch.index_select(
+                testing_features,
+                0,
+                torch.as_tensor(validation_indices_2, dtype=torch.int64),
+            ),
+        ]
+    )
 
     return clade_training_features, clade_testing_features, validation_features
+
 
 def format_data(data_dir, left_out_clade):
     adj = load_adjacency_matrix(data_dir, degree_normalised=False)
     adj = adj.coalesce()
     indices = adj.indices().tolist()
 
-    edges = set([(indices[0][i], indices[1][i]) 
-                    for i in range(len(indices[0]))])
+    edges = set(
+        [(indices[0][i], indices[1][i]) for i in range(len(indices[0]))]
+    )
 
     training_features = load_training_data(data_dir)[0]
     testing_features = load_testing_data(data_dir)[0]
     training_metadata, testing_metadata = load_metadata(data_dir)
 
     def parse_features(f):
-        return {i:f[i] for i in range(len(f))}
+        return {i: f[i] for i in range(len(f))}
 
-    features = train_test_validation_split(left_out_clade, training_features, 
-                                testing_features, training_metadata, 
-                                testing_metadata)
-    
+    features = train_test_validation_split(
+        left_out_clade,
+        training_features,
+        testing_features,
+        training_metadata,
+        testing_metadata,
+    )
+
     G_train = [[edges, parse_features(i.tolist())] for i in features[0]]
     G_test = [[edges, parse_features(i.tolist())] for i in features[1]]
     G_validate = [[edges, parse_features(i.tolist())] for i in features[2]]
 
-    return {'G_train': G_train, 'G_test': G_test, 'G_validate': G_validate}
+    return {"G_train": G_train, "G_test": G_test, "G_validate": G_validate}
+
 
 def fit_wl_kernel(G_train):
     wl_kernel = grakel.kernels.weisfeiler_lehman.WeisfeilerLehman(
-                                                            verbose = True,
-                                                            normalize = False)
+        verbose=True, normalize=False
+    )
     k_train = wl_kernel.fit_transform(G_train)
-    logging.info('Fitted WL kernel to training data and saved kernel')
+    logging.info("Fitted WL kernel to training data and saved kernel")
 
     return wl_kernel, k_train
 
+
 def transform_testing_data(wl_kernel, G_test):
     k_test = wl_kernel.transform(G_test)
-    logging.info('Transformed testing data')
+    logging.info("Transformed testing data")
 
-    return k_test 
+    return k_test
+
 
 def save(out_dir, left_out_clade, G_train, G_test, G_validate):
-    with open(os.path.join(out_dir, f'clade_{left_out_clade}_left_out_training_data_grakel_representation.pkl'), 'wb') as a:
+    with open(
+        os.path.join(
+            out_dir,
+            f"clade_{left_out_clade}_left_out_training_data_grakel_representation.pkl",
+        ),
+        "wb",
+    ) as a:
         pickle.dump(G_train, a)
-    with open(os.path.join(out_dir, f'clade_{left_out_clade}_left_out_testing_data_grakel_representation.pkl'), 'wb') as a:
+    with open(
+        os.path.join(
+            out_dir,
+            f"clade_{left_out_clade}_left_out_testing_data_grakel_representation.pkl",
+        ),
+        "wb",
+    ) as a:
         pickle.dump(G_test, a)
-    with open(os.path.join(out_dir, f'clade_{left_out_clade}_left_out_validation_data_grakel_representation.pkl'), 'wb') as a:
+    with open(
+        os.path.join(
+            out_dir,
+            f"clade_{left_out_clade}_left_out_validation_data_grakel_representation.pkl",
+        ),
+        "wb",
+    ) as a:
         pickle.dump(G_validate, a)
+
 
 def formatting_complete(out_dir, left_out_clade):
     formatted_files = []
-    formatted_files.append(os.path.join(out_dir, 
-        f'clade_{left_out_clade}_left_out_training_data_grakel_representation.pkl'))
-    formatted_files.append(os.path.join(out_dir, 
-         f'clade_{left_out_clade}_left_out_testing_data_grakel_representation.pkl'))
-    formatted_files.append(os.path.join(out_dir, 
-        f'clade_{left_out_clade}_left_out_validation_data_grakel_representation.pkl'))
+    formatted_files.append(
+        os.path.join(
+            out_dir,
+            f"clade_{left_out_clade}_left_out_training_data_grakel_representation.pkl",
+        )
+    )
+    formatted_files.append(
+        os.path.join(
+            out_dir,
+            f"clade_{left_out_clade}_left_out_testing_data_grakel_representation.pkl",
+        )
+    )
+    formatted_files.append(
+        os.path.join(
+            out_dir,
+            f"clade_{left_out_clade}_left_out_validation_data_grakel_representation.pkl",
+        )
+    )
 
     if all([os.path.isfile(i) for i in formatted_files]):
         return True
     else:
         return False
 
+
 def load_formatted_data(out_dir, left_out_clade):
-    with open(os.path.join(out_dir, f'clade_{left_out_clade}_left_out_training_data_grakel_representation.pkl'), 'rb') as a:
+    with open(
+        os.path.join(
+            out_dir,
+            f"clade_{left_out_clade}_left_out_training_data_grakel_representation.pkl",
+        ),
+        "rb",
+    ) as a:
         G_train = pickle.load(a)
-    with open(os.path.join(out_dir, f'clade_{left_out_clade}_left_out_testing_data_grakel_representation.pkl'), 'rb') as a:
+    with open(
+        os.path.join(
+            out_dir,
+            f"clade_{left_out_clade}_left_out_testing_data_grakel_representation.pkl",
+        ),
+        "rb",
+    ) as a:
         G_test = pickle.load(a)
-    with open(os.path.join(out_dir, f'clade_{left_out_clade}_left_out_validation_data_grakel_representation.pkl'), 'rb') as a:
+    with open(
+        os.path.join(
+            out_dir,
+            f"clade_{left_out_clade}_left_out_validation_data_grakel_representation.pkl",
+        ),
+        "rb",
+    ) as a:
         G_validate = pickle.load(a)
-    
+
     return G_train, G_test, G_validate
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     Ab = sys.argv[1]
     left_out_clade = int(sys.argv[2])
     fit = True
 
-    root_dir = 'data/model_inputs/freq_5_95/'
-    out_dir = f'kernel_learning/{Ab}/gwas_filtered/cluster_CV'
+    root_dir = "data/model_inputs/freq_5_95/"
+    out_dir = f"kernel_learning/{Ab}/gwas_filtered/cluster_CV"
 
     pre_formatted = formatting_complete(out_dir, left_out_clade)
 
@@ -133,33 +214,50 @@ if __name__ == '__main__':
         if not os.path.isdir(out_dir):
             os.makedirs(out_dir)
 
-        data_dir = os.path.join(root_dir, Ab, 'gwas_filtered')
+        data_dir = os.path.join(root_dir, Ab, "gwas_filtered")
         data_by_left_out_cluster = format_data(data_dir, left_out_clade)
 
-        G_train = data_by_left_out_cluster['G_train']
-        G_test = data_by_left_out_cluster['G_test']
-        G_validate = data_by_left_out_cluster['G_validate']
+        G_train = data_by_left_out_cluster["G_train"]
+        G_test = data_by_left_out_cluster["G_test"]
+        G_validate = data_by_left_out_cluster["G_validate"]
 
         save(out_dir, left_out_clade, G_train, G_test, G_validate)
 
     if fit:
         if pre_formatted:
-            G_train, G_test, G_train = load_formatted_data(out_dir, 
-                                                        left_out_clade)
+            G_train, G_test, G_validate = load_formatted_data(
+                out_dir, left_out_clade
+            )
         wl_kernel, k_train = fit_wl_kernel(G_train)
-        with open(os.path.join(out_dir, 
-            f'clade_{left_out_clade}_left_out_k_train.pkl'), 'wb') as a:
+        with open(
+            os.path.join(
+                out_dir, f"clade_{left_out_clade}_left_out_k_train.pkl"
+            ),
+            "wb",
+        ) as a:
             pickle.dump(k_train, a)
-        with open(os.path.join(out_dir,
-            f'clade_{left_out_clade}_left_out_wl_kernel.pkl'), 'wb') as a:
+        with open(
+            os.path.join(
+                out_dir, f"clade_{left_out_clade}_left_out_wl_kernel.pkl"
+            ),
+            "wb",
+        ) as a:
             pickle.dump(wl_kernel, a)
 
         k_test = transform_testing_data(wl_kernel, G_test)
-        with open(os.path.join(out_dir,
-            f'clade_{left_out_clade}_left_out_k_test.pkl'), 'wb') as a:
+        with open(
+            os.path.join(
+                out_dir, f"clade_{left_out_clade}_left_out_k_test.pkl"
+            ),
+            "wb",
+        ) as a:
             pickle.dump(k_test, a)
 
         k_validate = transform_testing_data(wl_kernel, G_validate)
-        with open(os.path.join(out_dir,
-            f'clade_{left_out_clade}_left_out_k_validate.pkl'), 'wb') as a:
+        with open(
+            os.path.join(
+                out_dir, f"clade_{left_out_clade}_left_out_k_validate.pkl"
+            ),
+            "wb",
+        ) as a:
             pickle.dump(k_validate, a)
--- scripts/plot_cv_results.py
+++ scripts/plot_cv_results.py
@@ -7,143 +7,233 @@ import re
 
 
 def load_lasso_data(data_dir):
-    if not data_dir.endswith('/'):
-        data_dir += '/'
-    input_files = glob.glob(data_dir + 'log2*pkl')
-    Abs = [os.path.split(i)[-1].split('_mic')[0] + '_mic' for i in input_files]
+    if not data_dir.endswith("/"):
+        data_dir += "/"
+    input_files = glob.glob(data_dir + "log2*pkl")
+    Abs = [os.path.split(i)[-1].split("_mic")[0] + "_mic" for i in input_files]
     data_dict = {}
     for i in range(len(Abs)):
-        with open(input_files[i], 'rb') as a:
+        with open(input_files[i], "rb") as a:
             data_dict[Abs[i]] = pickle.load(a)
     return data_dict
 
 
-def convert_to_dataframe(CV_results):
+def convert_to_dataframe_lasso(CV_results):
+    """
+    For formatting the output of the lasso model
+    """
+
     df_dictionary = {
-            'left_out_clade' : [],
-            'training_accuracy' : [],
-            'testing_accuracy' : [],
-            'validation_accuracy' : []
-    } #will be converted to df
+        "left_out_clade": [],
+        "training_accuracy": [],
+        "testing_accuracy": [],
+        "validation_accuracy": [],
+    }  # will be converted to df
     for left_out_clade, d in CV_results.items():
-        d_val = {i:d[i]['validation_accuracy'] for i in d.keys()}
-        k = max(d_val, key = d_val.get) #key with max validation data accuracy
-        df_dictionary['left_out_clade'].append(left_out_clade)
-        df_dictionary['training_accuracy'].append(d[k]['training_accuracy'])
-        df_dictionary['testing_accuracy'].append(d[k]['testing_accuracy'])
-        df_dictionary['validation_accuracy'].append(d[k]['validation_accuracy'])
-    
+        d_val = {i: d[i]["validation_accuracy"] for i in d.keys()}
+        k = max(d_val, key=d_val.get)  # key with max validation data accuracy
+        df_dictionary["left_out_clade"].append(left_out_clade)
+        df_dictionary["training_accuracy"].append(d[k]["training_accuracy"])
+        df_dictionary["testing_accuracy"].append(d[k]["testing_accuracy"])
+        df_dictionary["validation_accuracy"].append(
+            d[k]["validation_accuracy"]
+        )
+
     return pd.DataFrame(df_dictionary)
 
 
-def get_NN_results(data_dir, file_suffix = '.tsv'):
-    '''
-    Returns data from NN fitting metrics in form to be plotted with plot_results
-    '''
-    if not data_dir.endswith('/'):
-        data_dir += '/'
+def convert_to_dataframe(CV_results):
+    df_dictionary = {
+        "left_out_clade": [],
+        "training_accuracy": [],
+        "testing_accuracy": [],
+        "validation_accuracy": [],
+    }  # will be converted to df
+    for left_out_clade, d in CV_results.items():
+        df_dictionary["left_out_clade"].append(left_out_clade)
+        df_dictionary["training_accuracy"].append(
+            d["accuracies"][0]["training_accuracy"]
+        )
+        df_dictionary["testing_accuracy"].append(
+            d["accuracies"][0]["testing_accuracy"]
+        )
+        df_dictionary["validation_accuracy"].append(
+            d["accuracies"][0]["validation_accuracy"]
+        )
 
-    input_files = glob.glob(data_dir + f'*{file_suffix}')
-    Abs = [os.path.split(i)[-1].split('_mic')[0] + '_mic' for i in input_files]
-    
+    return pd.DataFrame(df_dictionary)
+
+
+def _get_NN_results(data_dir, file_suffix=".tsv"):
+    """
+    Returns data from NN fitting metrics in form to be plotted with plot_results
+    """
+    if not data_dir.endswith("/"):
+        data_dir += "/"
+
+    input_files = glob.glob(data_dir + f"*{file_suffix}")
+    Abs = [os.path.split(i)[-1].split("_mic")[0] + "_mic" for i in input_files]
+
     data_dict = {}
     for Ab in set(Abs):
         Ab_files = [i for i in input_files if i.startswith(data_dir + Ab)]
         Ab_accuracies = [None] * len(Ab_files)
         i = 0
         for f in Ab_files:
-            left_out_clade = re.search(r'clade_(.*?)_left', f).group(1)
-            last_line = pd.read_csv(Ab_files[0], sep = '\t').iloc[-1]
-            accuracies = last_line[['training_data_acc', 
-                                    'testing_data_acc',
-                                    'validation_data_acc']]
-            accuracies['left_out_clade'] = int(left_out_clade)
+            left_out_clade = re.search(r"clade_(.*?)_left", f).group(1)
+            last_line = pd.read_csv(Ab_files[0], sep="\t").iloc[-1]
+            accuracies = last_line[
+                [
+                    "training_data_acc",
+                    "testing_data_acc",
+                    "validation_data_acc",
+                ]
+            ]
+            accuracies["left_out_clade"] = int(left_out_clade)
             Ab_accuracies[i] = accuracies
             i += 1
-        df = pd.concat(Ab_accuracies, axis = 1).transpose()
-        df.set_index('left_out_clade', inplace = True)
-        df.columns = ['training_accuracy', 
-                    'testing_accuracy',
-                    'validation_accuracy']
+        df = pd.concat(Ab_accuracies, axis=1).transpose()
+        df.set_index("left_out_clade", inplace=True)
+        df.columns = [
+            "training_accuracy",
+            "testing_accuracy",
+            "validation_accuracy",
+        ]
         data_dict[Ab] = df
 
     return data_dict
 
 
 def get_SVR_results(dir_pattern):
-    Abs = ['log2_azm_mic', 
-            'log2_cip_mic',
-            'log2_cro_mic',
-            'log2_cfx_mic']
+    Abs = ["log2_azm_mic", "log2_cip_mic", "log2_cro_mic", "log2_cfx_mic"]
 
     input_files = [
-        os.path.join(dir_pattern[0], Ab, dir_pattern[1]) for Ab in Abs]
+        os.path.join(dir_pattern[0], Ab, dir_pattern[1]) for Ab in Abs
+    ]
 
     data_dict = {}
     for i in range(len(Abs)):
-        with open(input_files[i], 'rb') as a:
+        with open(input_files[i], "rb") as a:
             data_dict[Abs[i]] = pickle.load(a)
     return data_dict
 
 
+def density_plot(preds, labels, axs=None, axs_0=None, axs_1=None):
+    df = pd.DataFrame({"Predictions": preds, "Truth": labels})
+    if axs is not None:
+        df.plot(kind="kde", ax=axs[axs_0, axs_1], legend=False)
+        return axs
+    else:
+        df.plot(kind="kde")
+
+
+def plot_train_preds_and_labels(data, Ab):
+    fig, axs = plt.subplots(4, 3, sharex=True, sharey=True)
+    n = 0
+    m = 0
+    for left_out_clade, d in data[Ab].items():
+        axs = density_plot(
+            d["model_outputs"]["training_predictions"],
+            d["model_outputs"]["training_labels"],
+            axs=axs,
+            axs_0=n,
+            axs_1=m,
+        )
+
+        # axs = density_plot(d['model_outputs']['testing_predictions'],
+        #                     d['model_outputs']['testing_labels'],
+        #                     axs = axs,
+        #                     axs_0 = n,
+        #                     axs_1 = 1)
+
+        # axs = density_plot(d['model_outputs']['validation_predictions'],
+        #                     d['model_outputs']['validation_labels'],
+        #                     axs = axs,
+        #                     axs_0 = n,
+        #                     axs_1 = 2)
+
+        if n > 0 and n % 3 == 0:
+            n = 0
+            m += 1
+        else:
+            n += 1
+    fig.tight_layout()
+
+
 def bar_plot_of_results(data, filename):
-    fig, axs = plt.subplots(1, 4, sharey = True)
+    fig, axs = plt.subplots(1, 4, sharey=True)
     width = 0.2
     n = 0
     for Ab in data.keys():
         df = data[Ab]
-        axs[n].bar(df.index - width, df.training_accuracy, width, 
-                label = 'Training Data')
-        axs[n].bar(df.index, df.testing_accuracy, width, 
-                label = 'Testing Data')
-        axs[n].bar(df.index + width, df.validation_accuracy, width, 
-                label = 'Validation Data')
+        axs[n].bar(
+            df.index - width,
+            df.training_accuracy,
+            width,
+            label="Training Data",
+        )
+        axs[n].bar(df.index, df.testing_accuracy, width, label="Testing Data")
+        axs[n].bar(
+            df.index + width,
+            df.validation_accuracy,
+            width,
+            label="Validation Data",
+        )
         axs[n].set_xticks(df.index)
-        axs[n].set_title(Ab.upper().split('_')[1])
+        axs[n].set_title(Ab.upper().split("_")[1])
         n += 1
 
-    axs[n -1].legend(loc = 'lower right')
-    fig.text(0.5, 0.04, 'Left Out Clade', ha='center')
-    fig.text(0.04, 0.5, 'Prediction Accuracy (%)', 
-            va='center', rotation='vertical')
+    axs[n - 1].legend(loc="lower right")
+    fig.text(0.5, 0.04, "Left Out Clade", ha="center")
+    fig.text(
+        0.04, 0.5, "Prediction Accuracy (%)", va="center", rotation="vertical"
+    )
 
     fig.savefig(filename)
 
 
 def box_plot_of_results(data, filename):
-    fig, axs = plt.subplots(1, 4, sharey = True)
-    width = 0.2
+    fig, axs = plt.subplots(1, 4, sharey=True)
     n = 0
     for Ab in data.keys():
         df = data[Ab]
-        df = df.rename(columns = {'training_accuracy':'Train', 
-                                'testing_accuracy':'Test', 
-                                'validation_accuracy':'Validate'})
-        axs[n].boxplot(df, notch = False, labels = df.columns)
-        axs[n].set_title(Ab.upper().split('_')[1])
-        axs[n].tick_params(labelrotation = 90)
+        df = df.rename(
+            columns={
+                "training_accuracy": "Train",
+                "testing_accuracy": "Test",
+                "validation_accuracy": "Validate",
+            }
+        )
+        axs[n].boxplot(df, notch=False, labels=df.columns)
+        axs[n].set_title(Ab.upper().split("_")[1])
+        axs[n].tick_params(labelrotation=90)
         n += 1
 
-    fig.text(0, 0.5, 'Mean Accuracy per MIC Bin', 
-            va='center', rotation='vertical')
+    fig.text(
+        0, 0.5, "Mean Accuracy per MIC Bin", va="center", rotation="vertical"
+    )
     fig.tight_layout()
 
     fig.savefig(filename)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # data_dir = 'lasso_model/results/linear_model_results/cross_validation_results/'
     # data = load_lasso_data(data_dir)
 
-    #svr results are stored within subdir with name of Ab, unlike rest of results which are all stored in one dir
-    dir_pattern = ('kernel_learning', 
-            'cross_validation/SVR_results/SVR_CV_results_mean_acc_per_bin.pkl')
-    data_dir = '.'
+    # svr results are stored within subdir with name of Ab, unlike rest of results which are all stored in one dir
+    dir_pattern = (
+        "kernel_learning",
+        "cluster_wise_cross_validation/SVR_results/SVR_CV_results_mean_acc_per_bin.pkl",
+    )
+    data_dir = "."
     data = get_SVR_results(dir_pattern)
 
     Abs = list(data.keys())
-    Abs.sort() #to maintain order
-    data = {Ab:convert_to_dataframe(data[Ab]) for Ab in Abs}
-    data = {Ab:df.set_index('left_out_clade') for Ab, df in data.items()}
+    Abs.sort()  # to maintain order
+    accuracy_data = {Ab: convert_to_dataframe(data[Ab]) for Ab in Abs}
+    accuracy_data = {
+        Ab: df.set_index("left_out_clade") for Ab, df in accuracy_data.items()
+    }
 
-    box_plot_of_results(data, os.path.join(data_dir, 'CV_accuracy.png'))
+    box_plot_of_results(data, os.path.join(data_dir, "CV_accuracy.png"))
