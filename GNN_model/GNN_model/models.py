21c870c9c5a4b3d44c94aa8c29c1e877ac700561
Ben <bjeffrey92@hotmail.com>
2021-03-16 10:53:03 +0000

    formatting

--- GNN_model/GNN_model/models.py
+++ GNN_model/GNN_model/models.py
@@ -1,8 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from GNN_model.layers import GraphConvolution, GraphConvolutionPerNode, \
-                            PreSelectionConvolution, SparsePrePrunedLayer
+from GNN_model.layers import (
+    GraphConvolution,
+    GraphConvolutionPerNode,
+    PreSelectionConvolution,
+    SparsePrePrunedLayer,
+)
 
 
 class GraphConnectionsNN(nn.Module):
@@ -15,13 +19,13 @@ class GraphConnectionsNN(nn.Module):
         self.layer_4 = nn.Linear(n_hid_2, out_dim)
         self.dropout = dropout
 
-    def forward(self, model_input, adj = None):
+    def forward(self, model_input, adj=None):
         x = F.leaky_relu(self.layer_1(model_input))
-        F.dropout(x, self.dropout, inplace = True, training = True)
+        F.dropout(x, self.dropout, inplace=True, training=True)
         x = F.leaky_relu(self.layer_2(x))
-        F.dropout(x, self.dropout, inplace = True, training = True)
+        F.dropout(x, self.dropout, inplace=True, training=True)
         x = F.leaky_relu(self.layer_3(x))
-        F.dropout(x, self.dropout, inplace = True, training = True)
+        F.dropout(x, self.dropout, inplace=True, training=True)
         out = F.leaky_relu(self.layer_4(x))[0]
         return out
 
@@ -29,14 +33,14 @@ class GraphConnectionsNN(nn.Module):
 class GraphEdgeWiseAttention(nn.Module):
     def __init__(self, adj, n_nodes, out_dim):
         super(GraphEdgeWiseAttention, self).__init__()
-        
+
         self.n_nodes = n_nodes
         self.out_dim = out_dim
         adj = adj.coalesce()
         self.w = nn.Parameter(torch.empty(size=(len(adj.values()), 1)))
         nn.init.uniform_(self.w.data, 0, 1)
-        self.adj = torch.sparse_coo_tensor(adj.indices(), self.w.squeeze(1)) 
-        self.ones = torch.ones((out_dim,n_nodes))
+        self.adj = torch.sparse_coo_tensor(adj.indices(), self.w.squeeze(1))
+        self.ones = torch.ones((out_dim, n_nodes))
 
     def forward(self, layer_input):
         x = torch.relu(torch.sparse.mm(self.adj, layer_input))
@@ -44,9 +48,15 @@ class GraphEdgeWiseAttention(nn.Module):
         return out
 
     def __repr__(self):
-        return self.__class__.__name__ + ' (' \
-               + str(self.n_nodes) + ' -> ' \
-               + str(self.out_dim) + ')'
+        return (
+            self.__class__.__name__
+            + " ("
+            + str(self.n_nodes)
+            + " -> "
+            + str(self.out_dim)
+            + ")"
+        )
+
 
 class GCNGlobalNode(nn.Module):
     def __init__(self, n_feat, conv_1, n_hid, out_dim, dropout):
@@ -59,64 +69,73 @@ class GCNGlobalNode(nn.Module):
         self.dropout = dropout
 
     def forward(self, x, adj):
-        x = F.leaky_relu(self.gc1(x, adj))[-1] #select last node which is the global node
-        # x = F.leaky_relu(self.gc2(x, adj))[-1] 
+        x = F.leaky_relu(self.gc1(x, adj))[
+            -1
+        ]  # select last node which is the global node
+        # x = F.leaky_relu(self.gc2(x, adj))[-1]
         x = F.leaky_relu(self.linear1(x))
-        F.dropout(x, self.dropout, inplace = True, training = True)
+        F.dropout(x, self.dropout, inplace=True, training=True)
         out = F.leaky_relu(self.linear2(x))[0]
         return out
 
 
 class GCNMaxPooling(nn.Module):
-    def __init__(self, n_feat, conv_1, conv_2 = 0, n_hid_1 = 0, n_hid_2 = 0, 
-                out_dim = 1, dropout = 0):
+    def __init__(
+        self,
+        n_feat,
+        conv_1,
+        conv_2=0,
+        n_hid_1=0,
+        n_hid_2=0,
+        out_dim=1,
+        dropout=0,
+    ):
         super(GCNMaxPooling, self).__init__()
 
         self.gc1 = GraphConvolution(n_feat, conv_1)
-        
-        #if more than one convolutional layer
+
+        # if more than one convolutional layer
         if conv_2 > 0:
             self.gc2 = GraphConvolution(n_feat, conv_2)
         else:
             self.gc2 = None
-        
+
         self.linear1 = nn.Linear(n_feat, n_hid_1)
-        
-        #if more than one densely connected hidden layer
+
+        # if more than one densely connected hidden layer
         if n_hid_2 > 0:
             self.linear2 = nn.Linear(n_hid_1, n_hid_2)
             self.linear3 = nn.Linear(n_hid_2, out_dim)
         else:
             self.linear2 = nn.Linear(n_hid_1, out_dim)
             self.linear3 = None
-        
+
         self.dropout = dropout
 
     def forward(self, x, adj):
         x = F.leaky_relu(self.gc1(x, adj))
-        
+
         if self.gc2 is not None:
-            x, _ = torch.max(x, 1) #global max pooling
+            x, _ = torch.max(x, 1)  # global max pooling
             x = x.unsqueeze(1)
             x = F.leaky_relu(self.gc2(x, adj))
-        
-        x, _ = torch.max(x, 1) #global max pooling
+
+        x, _ = torch.max(x, 1)  # global max pooling
         x = x.unsqueeze(1)
-        
+
         x = F.leaky_relu(self.linear1(x))
-        F.dropout(x, self.dropout, inplace = True, training = True)
-        
+        F.dropout(x, self.dropout, inplace=True, training=True)
+
         x = F.leaky_relu(self.linear2(x))
         if self.linear3 is not None:
-            F.dropout(x, self.dropout, inplace = True, training = True)
+            F.dropout(x, self.dropout, inplace=True, training=True)
             x = self.linear3(x)
 
         return x[0][0]
 
 
 class GCNPerNode(nn.Module):
-    def __init__(self, n_feat, n_hid_1, n_hid_2, out_dim, dropout, 
-                n_hid_3 = 0):
+    def __init__(self, n_feat, n_hid_1, n_hid_2, out_dim, dropout, n_hid_3=0):
         super(GCNPerNode, self).__init__()
 
         self.gc = GraphConvolutionPerNode(n_feat, n_hid_1)
@@ -132,20 +151,20 @@ class GCNPerNode(nn.Module):
 
     def _initialise_weights(self):
         for name, param in self.named_parameters():
-            if name.endswith('weight'): #if weight
+            if name.endswith("weight"):  # if weight
                 nn.init.xavier_uniform_(param)
 
     def forward(self, x, adj):
         x = self.gc(x, adj)
         x = torch.tanh(x).clone()
-        F.dropout(x, self.dropout, inplace = True, training = True)
+        F.dropout(x, self.dropout, inplace=True, training=True)
         x = self.linear1(x)
         x = torch.tanh(x).clone()
-        F.dropout(x, self.dropout, inplace = True, training = True)
+        F.dropout(x, self.dropout, inplace=True, training=True)
         if self.linear3 is not None:
             x = self.linear2(x)
             x = torch.tanh(x).clone()
-            F.dropout(x, self.dropout, inplace = True, training = True)
+            F.dropout(x, self.dropout, inplace=True, training=True)
             out = self.linear3(x)[0][0]
         else:
             out = self.linear2(x)[0][0]
@@ -153,18 +172,18 @@ class GCNPerNode(nn.Module):
 
 
 class PreConvolutionNN(nn.Module):
-    def __init__(self, n_nodes, n_neighbours, dropout, out_dim = 1):
+    def __init__(self, n_nodes, n_neighbours, dropout, out_dim=1):
         super(PreConvolutionNN, self).__init__()
 
         self.conv = PreSelectionConvolution(n_nodes, n_neighbours)
         self.linear1 = nn.Linear(n_nodes, out_dim)
         self.dropout = dropout
 
-    #adj included for consistency with training functions, simplifies training
-    def forward(self, x, adj = None): 
+    # adj included for consistency with training functions, simplifies training
+    def forward(self, x, adj=None):
         x = F.leaky_relu(self.conv(x)).unsqueeze(1)
-        F.dropout(x, self.dropout, inplace = True, training = True)
-        out = self.linear1(x.transpose(0,1))[0][0]
+        F.dropout(x, self.dropout, inplace=True, training=True)
+        out = self.linear1(x.transpose(0, 1))[0][0]
         return out
 
 
@@ -172,9 +191,10 @@ class LinearConv(nn.Module):
     def __init__(self, k, n_nodes):
         super(LinearConv, self).__init__()
         self.linear1 = nn.Linear(k * n_nodes, 1)
-    def forward(self, x, adj = None):
+
+    def forward(self, x, adj=None):
         x = x.flatten(0).unsqueeze(1)
-        return self.linear1(x.transpose(0,1))[0][0]
+        return self.linear1(x.transpose(0, 1))[0][0]
 
 
 class VanillaNN(nn.Module):
@@ -186,11 +206,12 @@ class VanillaNN(nn.Module):
         self.linear3 = nn.Linear(n_hid_2, out_dim)
         self.dropout = dropout
 
-    def forward(self, x, adj = None): #add unused adj argument so same training functions can be used for now
-        x = F.leaky_relu(self.linear1(x.transpose(0,1)))
-        F.dropout(x, self.dropout, inplace = True, training = True)
+    def forward(
+        self, x, adj=None
+    ):  # add unused adj argument so same training functions can be used for now
+        x = F.leaky_relu(self.linear1(x.transpose(0, 1)))
+        F.dropout(x, self.dropout, inplace=True, training=True)
         x = F.leaky_relu(self.linear2(x))
-        F.dropout(x, self.dropout, inplace = True, training = True)
+        F.dropout(x, self.dropout, inplace=True, training=True)
         out = F.leaky_relu(self.linear3(x)[0][0])
         return out
-
