b887d96eb398ae2bc80ae488660e95fca068faf1
Ben <bjeffrey92@hotmail.com>
2021-03-18 09:14:29 +0000

    renaming files

---
+++ lasso_model/lasso_model/elastic_net.py
@@ -0,0 +1,216 @@
+import pickle
+import os
+import logging
+import warnings
+from functools import partial
+
+import torch
+from numpy import sort, array_equal
+from sklearn.linear_model import ElasticNet
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.metrics import mean_squared_error
+from bayes_opt import BayesianOptimization
+
+from lasso_model.utils import (
+    load_training_data,
+    load_testing_data,
+    mean_acc_per_bin,
+    load_metadata,
+)
+
+
+logging.basicConfig()
+logging.root.setLevel(logging.INFO)
+
+
+def train_evaluate(
+    training_features,
+    training_labels,
+    testing_features,
+    testing_labels,
+    validation_features,
+    validation_labels,
+    alpha,
+    l1_ratio,
+):
+
+    max_iter = 100000
+    fitted = False
+    while not fitted:
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+
+            reg = ElasticNet(
+                alpha=alpha,
+                l1_ratio=l1_ratio,
+                random_state=0,
+                max_iter=max_iter,
+            )
+            reg.fit(training_features, training_labels)
+
+            if len(w) > 1:
+                for warning in w:
+                    logging.error(warning.category)
+                raise Exception
+            elif w and issubclass(w[0].category, ConvergenceWarning):
+                logging.warning(
+                    f"Failed to converge with max_iter = {max_iter}, adding 1000 more"
+                )
+                max_iter += 1000
+            else:
+                fitted = True
+
+    if validation_features is None:
+        testing_predictions = reg.predict(testing_features)
+        testing_loss = float(
+            mean_squared_error(testing_labels, testing_predictions)
+        )
+        return -testing_loss
+    else:
+        training_predictions = reg.predict(training_features)
+        testing_predictions = reg.predict(testing_features)
+        validation_predictions = reg.predict(validation_features)
+
+        training_accuracy = mean_acc_per_bin(
+            torch.as_tensor(training_predictions), training_labels
+        )
+        testing_accuracy = mean_acc_per_bin(
+            torch.as_tensor(testing_predictions), testing_labels
+        )
+        validation_accuracy = mean_acc_per_bin(
+            torch.as_tensor(validation_predictions), validation_labels
+        )
+
+        accuracy_dict = {
+            "training_accuracy": training_accuracy,
+            "testing_accuracy": testing_accuracy,
+            "validation_accuracy": validation_accuracy,
+        }
+        logging.info(accuracy_dict)
+
+        return accuracy_dict
+
+
+def leave_one_out_CV(
+    training_data, testing_data, training_metadata, testing_metadata
+):
+
+    clades = sort(training_metadata.clusters.unique())
+    assert array_equal(
+        sort(clades), sort(testing_metadata.clusters.unique())
+    ), "Different clades found in training and testing metadata"
+
+    results_dict = {}
+    for left_out_clade in clades:
+        logging.info(
+            f"Formatting data for model with clade {left_out_clade} left out"
+        )
+        training_indices = training_metadata.loc[
+            training_metadata.clusters != left_out_clade
+        ].index
+        testing_indices = testing_metadata.loc[
+            testing_metadata.clusters != left_out_clade
+        ].index
+        validation_indices_1 = training_metadata.loc[
+            training_metadata.clusters == left_out_clade
+        ].index  # extract data from training set
+        validation_indices_2 = testing_metadata.loc[
+            testing_metadata.clusters == left_out_clade
+        ].index  # extract data from testing set
+
+        training_features = torch.index_select(
+            training_data[0], 0, torch.as_tensor(training_indices)
+        )
+        training_labels = torch.index_select(
+            training_data[1], 0, torch.as_tensor(training_indices)
+        )
+        testing_features = torch.index_select(
+            testing_data[0], 0, torch.as_tensor(testing_indices)
+        )
+        testing_labels = torch.index_select(
+            testing_data[1], 0, torch.as_tensor(testing_indices)
+        )
+        validation_features = torch.cat(
+            [
+                torch.index_select(
+                    training_data[0], 0, torch.as_tensor(validation_indices_1)
+                ),
+                torch.index_select(
+                    testing_data[0], 0, torch.as_tensor(validation_indices_2)
+                ),
+            ]
+        )
+        validation_labels = torch.cat(
+            [
+                torch.index_select(
+                    training_data[1], 0, torch.as_tensor(validation_indices_1)
+                ),
+                torch.index_select(
+                    testing_data[1], 0, torch.as_tensor(validation_indices_2)
+                ),
+            ]
+        )
+
+        pbounds = {"alpha": (0.2, 1.8), "l1_ratio": (0, 1)}
+
+        partial_fitting_function = partial(
+            train_evaluate,
+            training_features=training_features,
+            training_labels=training_labels,
+            testing_features=testing_features,
+            testing_labels=testing_labels,
+            validation_features=None,
+            validation_labels=None,
+        )
+
+        logging.info("Optimizing hyperparameters")
+        optimizer = BayesianOptimization(
+            f=partial_fitting_function, pbounds=pbounds, random_state=1
+        )
+        optimizer.maximize(n_iter=15)
+
+        logging.info(
+            "Optimization complete, extracting metrics for best hyperparameter combination"
+        )
+        results_dict[left_out_clade] = train_evaluate(
+            training_features,
+            training_labels,
+            testing_features,
+            testing_labels,
+            validation_features,
+            validation_labels,
+            optimizer.max["params"]["alpha"],
+            optimizer.max["params"]["l1_ratio"],
+        )
+
+    return results_dict
+
+
+def save_output(results_dict, results_dir, outcome):
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    fname = os.path.join(
+        results_dir, outcome + "_CV_elastic_net_predictions.pkl"
+    )
+    with open(fname, "wb") as a:
+        pickle.dump(results_dict, a)
+
+
+if __name__ == "__main__":
+    root_dir = "data/model_inputs/freq_5_95/"
+
+    outcomes = os.listdir(root_dir)
+    for outcome in outcomes:
+        logging.info(f"Fitting models with {outcome}")
+        data_dir = os.path.join(root_dir, outcome, "gwas_filtered")
+        results_dir = "lasso_model/results/elastic_net_results/gwas_filtered/cluster_wise_CV"
+
+        training_data = load_training_data(data_dir)
+        testing_data = load_testing_data(data_dir)
+        training_metadata, testing_metadata = load_metadata(data_dir)
+
+        results_dict = leave_one_out_CV(
+            training_data, testing_data, training_metadata, testing_metadata
+        )
+
+        save_output(results_dict, results_dir, outcome)
