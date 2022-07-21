1846c2b3c4bfd623665d0d89cb8b36632d7a4ca5
bjeffrey92 <bjeffrey92@hotmail.com>
2022-07-21 13:03:19 +0100

    parallel running get adj dict

--- decision_tree_models/decision_tree_models/utils.py
+++ decision_tree_models/decision_tree_models/utils.py
@@ -1,7 +1,11 @@
-from functools import lru_cache
+from functools import lru_cache, partial
+from multiprocessing import cpu_count, Pool
+from typing import Dict
 
 import numpy as np
 from torch import Tensor
+
+from decision_tree_models.parallel_get_indices import get_indices_parallel
 
 
 @lru_cache(maxsize=1)
@@ -17,6 +21,18 @@ def check_data_format(data: np.ndarray) -> np.ndarray:
 
 
 @lru_cache(maxsize=1)
-def adj_matrix_to_dict(adj: Tensor):
+def adj_matrix_to_dict(adj: Tensor, njobs: int = 1) -> Dict:
     indices = convert_adj_matrix(adj)
-    return {i: indices[1, indices[0] == i] for i in np.unique(indices[0])}
+    if njobs == 1:
+        return {i: indices[1, indices[0] == i] for i in np.unique(indices[0])}
+
+    if njobs == -1:
+        njobs = cpu_count()
+
+    unique_0_indices = np.unique(indices[0])
+    array_chunks = np.array_split(unique_0_indices, njobs)
+
+    pool = Pool(njobs)
+    results = pool.map(partial(get_indices_parallel, indices=indices), array_chunks)
+
+    return {k: v for d in results for k, v in d.items()}
